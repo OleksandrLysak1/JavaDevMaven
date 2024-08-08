1ece1c77c6c790613e7611b48721d053a88710dc
Oleksandr Lysak <lisak.aleksandr@gmail.com>
2024-08-08 22:20:44 +0200

    MavenProject 1.0

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Oleksandr";
+        String lastName = "Lysak";
+        System.out.println(name + " " + lastName);
+    }
+}
